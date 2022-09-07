a82291a88cae5a444f15e1109e743ae364d6f33a
Adrien Ghosn <ghosn.adrien@gmail.com>
2022-09-07 15:47:22 +0100

    Adding more details

--- wasm/my_snmalloc_test/src/main.cc
+++ wasm/my_snmalloc_test/src/main.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <sys/mman.h>
 #include <cstdlib>
-
 #include <snmalloc/snmalloc.h>
+
+#define PAGE_SIZE 0x1000
+#define NB_PAGES 4
+#define ALLOC_SIZE (PAGE_SIZE * NB_PAGES)
 
 void* mmap_region()
 {
-  void* result = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1 , 0);
+  void* result = mmap(NULL, ALLOC_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1 , 0);
   if (result == MAP_FAILED)
     abort();
   return result;
@@ -15,5 +18,17 @@ void* mmap_region()
 int main(void)
 {
   std::cout << "Hello world!" << std::endl;
+  void* my_region = mmap_region();
+
+  //  TODO: 
+  //  I want snmalloc to register my_region and manage allocations to/from it.
+  //  Ideally, I'd like to have an allocator object with the API:
+  //  class Allocator {
+  //    void* alloc(size_t size);
+  //    void dealloc(void* ptr);
+  //  };
+  //  Allocator should go through snmalloc to alloc/dealloc objects from my_region. 
+  //
+  //  HOW DO I DO THAT WITHOUT WRITTING 10K lines of code?
   return 0;
 }
